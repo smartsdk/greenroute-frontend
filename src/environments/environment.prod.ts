5273b3370da3feff5c315ce3cda26ed6f78532a4
samjm <sam.jm1618@gmail.com>
2018-07-19 13:28:47 -0500

    Fix sed: bad option in substitution expression and javascript reference

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  backend_sdk: 'http://localhost:8080/back-sdk',
-  alerts_url: 'http://localhost:8443/#/',
-  statistics_url: 'http://localhost:3000/dashboard/db/airquality-dashboard',
-  routingmap_url: 'http://localhost:8081/'
+  backend_sdk: 'GR_BACKEND_URL',
+  alerts_url: 'https://smartsdkitesm.com',
+  statistics_url: 'GR_GRAFANA_URL',
+  routingmap_url: 'http://greenroute.smartsdk.ubiwhere.com/'
 };
