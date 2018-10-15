3cd0c556be97d4baf996b9fcf5066930236c85b5
samjm <sam.jm1618@gmail.com>
2018-10-15 11:47:21 -0500

    sed expression update

--- start_front.sh
+++ start_front.sh
@@ -2,5 +2,7 @@
 
 sed -i "s|GR_BACKEND_URL|${GR_BACKEND_URL}|g" /usr/share/nginx/html/main.bundle.js
 sed -i "s|GR_GRAFANA_URL|${GR_GRAFANA_URL}|g" /usr/share/nginx/html/main.bundle.js
+sed -i "s|GR_ALERTS_URL|${GR_ALERTS_URL}|g" /usr/share/nginx/html/main.bundle.js
+sed -i "s|GR_ROUTINGMAP_URL|${GR_ROUTINGMAP_URL}|g" /usr/share/nginx/html/main.bundle.js
 
 nginx -g "daemon off;"
